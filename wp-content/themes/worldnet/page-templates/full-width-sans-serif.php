5ebee97967dfed291fc689b857a5307d971f5439
stripjenga <LydivetteRoldan@lydivette-MacBook-Pro.local>
2015-06-11 10:59:36 -0400

    Adding new body text styles, new sans-serif template.
    The new full-width sans-serif template is applied only to: Home, My Account, Customer Support, Product Portfolio and Knowledge Center.

--- wp-content/themes/worldnet/page-templates/full-width-sans-serif.php
+++ wp-content/themes/worldnet/page-templates/full-width-sans-serif.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: Right Sidebar With Featured Image
+ * Template Name: Full-width Page Template, No Sidebar - Sans-Serif
  *
  */
 
@@ -8,21 +8,16 @@ get_header(); ?>
 
 	<?php while ( have_posts() ) : the_post(); ?>
 	<h2><?php the_title(); ?></h2>
-	
 	<div class="cont_right_box big_width">
-
+		
 		<?php if ( has_post_thumbnail() ) : ?>
 				<div class="clear"></div>
-				<div style="float:right; margin-top:0; margin-bottom:0; margin-left:30px; margin-right:0; position:relative;">
+				<div style="float: right; margin-top:0px; margin-bottom:30px; margin-left:30px; position:relative;">
 					<div>
 						<img src="<?php echo wp_get_attachment_url( get_post_thumbnail_id(get_the_ID())); ?>" class="fr">
 					</div>
 					<div class="clear"></div>
-				<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
-	<div class="sidebar">
-		<?php dynamic_sidebar( 'sidebar-1' ); ?>
-	</div><!-- .first -->
-	<?php endif; ?>	
+					
 				</div>
 				
 		<?php endif; ?>
